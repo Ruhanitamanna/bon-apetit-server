a74cbbe61b07d03240a596b80756f0d45a59cac8
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-05-04 02:12:55 +0600

    data uploaded to the port

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const data = require('./data/data.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+
+  app.get('/data', (req, res)=>{
+    res.send(data);
   })
 
   app.listen(port, () => {
