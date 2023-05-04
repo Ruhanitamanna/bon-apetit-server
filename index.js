47c3861c1f8e3bb9ee269a22e7708bf2025b5691
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-05-04 21:13:50 +0600

    server side done

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const data = require('./data/data.json')
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
 
   app.get('/data', (req, res)=>{
     res.send(data);
